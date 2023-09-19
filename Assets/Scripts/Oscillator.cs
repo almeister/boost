72962258ca75c1819bab744166ce7c3a41de6848
Alex Coaton <alex.coaton@gmail.com>
2023-09-19 12:15:18 +1200

    5 more levels.  Webgl build.

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,6 +6,7 @@ public class Oscillator : MonoBehaviour
 {
   [SerializeField] Vector3 movementVector;
   [SerializeField] float periodSeconds = 2f;
+  [SerializeField] bool useBounce = false;
 
   private Vector3 _startingPosition;
   private float _movementFactor;
@@ -19,12 +20,23 @@ public class Oscillator : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    float cycles = Time.time / periodSeconds;
+    if (periodSeconds <= Mathf.Epsilon)
+    {
+      return;
+    }
+
     const float tau = Mathf.PI * 2;
+    float cycles = Time.time / periodSeconds;
     float rawSineWave = Mathf.Sin(cycles * tau);
 
     _movementFactor = (rawSineWave + 1f) / 2f; // So that movementFactor oscillates between 0 and 1
+    if (useBounce)
+    {
+      _movementFactor = Mathf.Pow(_movementFactor, 2f);
+    }
+
     Vector3 offset = movementVector * _movementFactor;
     transform.position = _startingPosition + offset;
+
   }
 }
